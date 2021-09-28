457b9b0c0167e4c54f3cfa1239535e6035f8b9b2
Michael Orr <mike@glquote.com>
2021-09-28 16:53:24 -0700

    game is functional, just need to add questions that are related to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var start = document.querySelector('.start');
 var startContainer = document.querySelector('.start-container');
 var quizContainer = document.querySelector('.quiz-container');
+var gameOverContainer = document.querySelector('.game-over-container');
 var timeLeft = document.querySelector('.time-left');
 var seconds = document.querySelector('.seconds');
 var highScoresContainer = document.querySelector('.high-scores-container');
@@ -9,7 +10,15 @@ var questionEl = document.getElementById('question');
 var answerButtonsEl = document.getElementById('answers');
 var timerEl = document.getElementById('seconds');
 var score = document.querySelector('.score');
-
+var playerNameInput = document.querySelector("#player");
+var highScoreForm = document.getElementById('high-score-entry');
+var scoreSheet = document.querySelector("#score-sheet");
+var mostRecent = document.querySelector('#most-recent');
+var highScoresButton = document.querySelector('.high-scores-button')
+var restart = document.querySelector('.restart');
+var reset = document.querySelector('.reset');
+var highScoreEl = document.querySelector('#high-score');
+var timerContainerEl = document.querySelector('.time-left');
 
 var questions = [
     {
@@ -115,7 +124,8 @@ var questions = [
 var currentQuestionIndex = 0;
 var correctAnswerIndex = 0;
 var timeLeft = 59;
-
+var lastScore = 0;
+var highScoresList = JSON.parse(localStorage.getItem("highScoresList"));
 
 
 function clearQuestion() {
@@ -131,9 +141,15 @@ function stopTimer() {
 
 function gameOver () {
     quizContainer.classList.add('hidden');
-    highScoresContainer.classList.remove('hidden');
+    gameOverContainer.classList.remove('hidden');
     stopTimer(); 
-    score.innerText = timeLeft + 1; 
+    if (timeLeft <= 0) {
+        score.innerText = 0; 
+        lastScore = 0;
+    } else {
+        score.innerText = timeLeft + 1; 
+        lastScore = timeLeft + 1;
+    };
 }
 
 var timer = '';
@@ -187,6 +203,7 @@ function answerQuestion(event) {
     if (!selectedAnswer.dataset.correct) {
         selectedAnswer.innerText = selectedAnswer.innerText + "  - WRONG ANSWER!!! -";
         selectedAnswer.classList.add('wrong');
+        timeLeft = timeLeft - 10;
     } else {
         selectedAnswer.innerText = selectedAnswer.innerText + "  - YOU ARE CORRECT!!! -";
     };
@@ -228,8 +245,16 @@ function firstQuestion() {
 
 
 function startQuiz() {
+    currentQuestionIndex = 0;
+    timeLeft = 60;
+    timerContainerEl.classList.remove('hidden');
     countdown();
+    restart.classList.add('hidden');
+    reset.classList.add('hidden');
     startContainer.classList.add("hidden");
+    highScoresButton.classList.add('hidden');
+    gameOverContainer.classList.add("hidden");
+    highScoresContainer.classList.add("hidden");
     quizContainer.classList.remove("hidden");
     firstQuestion();
     if (timeLeft <= 0) {
@@ -237,14 +262,100 @@ function startQuiz() {
     }
 }
 
+
+function renderLastScore() {
+    var player = localStorage.getItem("player");
+    var lastPlayersScore = localStorage.getItem("lastScore");
+    if (!player) {
+        return;
+    };
+    console.log(player + " " + lastPlayersScore); 
+    mostRecent.innerText = "Most Recent Score:   " + player + " - " + lastPlayersScore;
+}
+
+function renderHighScore() {
+    var highScore = [];
+    timerContainerEl.classList.add('hidden');
+    highScoresList = JSON.parse(localStorage.getItem("highScoresList"));
+    highScoresList.sort(function(a, b) {
+        return b[1] - a[1];
+      });
+    highScore = highScoresList[0];
+    highScoreEl.innerText = "High Score:   " + highScore[0] + " - " + highScore[1];
+}
+
+function clearScores() {
+    var highScoresList = [];
+    localStorage.setItem('highScoresList', JSON.stringify(highScoresList));
+    renderScoresList();
+}
+
+function renderScoresList() {
+    startContainer.classList.add('hidden');
+    restart.classList.remove('hidden');
+    reset.classList.remove('hidden');
+    highScoresButton.classList.add('hidden');
+    highScoresList = JSON.parse(localStorage.getItem("highScoresList"));
+    highScoresList.sort(function(a, b) {
+        return b[1] - a[1];
+      });
+    highScoresContainer.classList.remove('hidden');
+    gameOverContainer.classList.add('hidden');
+    console.log(highScoresList);
+    scoreSheet.innerText = '';
+    for (i = 0; i < 11; i++) {
+        var highScoresListItem = document.createElement('h3');
+        highScoresListItem.textContent = highScoresList[i][0] + " " + highScoresList[i][1];
+        console.log(highScoresListItem);
+        scoreSheet.appendChild(highScoresListItem);
+    }; 
+}
+
+renderLastScore();
+renderHighScore();
+
+highScoresButton.addEventListener('click', renderScoresList);
+
 start.addEventListener('click', startQuiz);
 
+restart.addEventListener('click', startQuiz);
+
+reset.addEventListener('click', clearScores);
+
+highScoreForm.addEventListener("submit", function (event) {
+    var player = document.querySelector("#player").value;
+    var highScoresListItem = [player, lastScore];
+    event.preventDefault();
+    console.log(highScoresListItem);
+    console.log(player);
+    console.log(lastScore);
+    localStorage.setItem("player", player);
+    localStorage.setItem("lastScore", lastScore);
+    highScoresList.push(highScoresListItem);
+    console.log(highScoresList);
+    localStorage.setItem('highScoresList', JSON.stringify(highScoresList));
+    renderLastScore();
+    renderScoresList();
+    renderHighScore();
+});
 
 
+
+//DONE
 //start the quiz//--
-//timer
-//set next question
+//timer//--
+//set next question//--
 //show result message / colors//--
 //evaulate if correct answers//--
-//subtract time for incorrect answers
-//end quiz when timer runs out.
+//subtract time for incorrect answers//--
+//end quiz when timer runs out.//--
+// make list of high scores - USE THE TODOS ACTIVITY AS AN EXAMPLE.
+//high score reset button
+//see high scores section (without playing game)
+//restart button on high scores section
+//sort high scores
+//show high score in corner
+
+//THINGS LEFT TO DO
+//style for mobile
+
